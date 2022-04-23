366fdc76842fceaafac3dd63b0065630bc491398
poap viking <dorihths@icloud.com>
2022-04-23 21:53:44 +0000

    rinkr

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x19BE2a39783683E4Fea33aE17eB74daa2Ad7e47c"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
