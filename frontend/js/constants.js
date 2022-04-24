a44db4d52f5616d6c96f1c1f48ecbaf2f93d3326
poap viking <dorihths@icloud.com>
2022-04-24 00:21:20 +0000

    hghghgh

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,10 +3,10 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "PixelFeels FAN ART Collection!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'PixelFeels is a collection of 2222 randomly generated Feels as a FAN ART with NO ROADMAP!! But everything can happen.';
+const welcome_p = 'PixelFeels is a collection of 2222 randomly generated Feels as a FAN ART with NO ROADMAP,  But everything can happen. PixelFeels is a FREE to mint Collection.';
 const h1_presale_coming_soon = "PixelFeelS Droping Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
+const h1_public_mint = "PixelFeels Minting NOW!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
