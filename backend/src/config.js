94a34b6f052834893d04912a497235e3a8bb8851
poap viking <dorihths@icloud.com>
2022-04-24 01:45:52 +0000

    sadasd

--- backend/src/config.js
+++ backend/src/config.js
@@ -93,7 +93,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x19BE2a39783683E4Fea33aE17eB74daa2Ad7e47c"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
